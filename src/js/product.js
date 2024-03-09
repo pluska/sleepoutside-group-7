30eb6476deb5eaba36b6fef05253054248e64217
Andres Pluska <pluskaandres@gmail.com>
2024-03-09 15:02:33 -0500

    Asignment 2

--- src/js/product.js
+++ src/js/product.js
@@ -1,20 +1,10 @@
-import { setLocalStorage, getLocalStorage } from "./utils.mjs";
+import { getParams } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
+const productId = getParams("product");
 const dataSource = new ProductData("tents");
 
-function addProductToCart(product) {
-  const products = getLocalStorage("so-cart") || [];
-  products.push(product);
-  setLocalStorage("so-cart", products);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+const product = new ProductDetails(productId, dataSource);
 
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+product.init();
